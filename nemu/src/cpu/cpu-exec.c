f24f5233273aacf8a2bb99e28ab4ee9e1809e2fb
catfood <protonghost@outlook.com>
2022-08-27 13:32:29 +0800

    am-kernels ics2021 initialized without tracing
    am-kernels adc316af6e482e6444a9bd68bafc3a57e2cafdbc cpu-tests,Makefile: support target `gdb`

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -24,7 +24,12 @@ void fetch_decode(Decode *s, vaddr_t pc);
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) log_write("%s\n", _this->logbuf);
+  #if ITRACE_COND
+    log_write("%s\n", _this->logbuf);
+  #endif
+  // Simple fix to make cpp analyzer happy
+  // if problems occur, use this original line below
+  // if (ITRACE_COND) log_write("%s\n", _this->logbuf);
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
