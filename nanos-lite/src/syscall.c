9b2db2fde4609a2d8182409a0397ee9d32836763
catfood <protonghost@outlook.com>
2022-09-06 00:43:10 +0800

    remove dependency on syscall.h

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,12 @@
 #include <common.h>
 #include "syscall.h"
 
+enum {
+  SYS_exit,   SYS_yield,  SYS_open,   SYS_read, SYS_write,  SYS_kill,   SYS_getpid, SYS_close,
+  SYS_lseek,  SYS_brk,    SYS_fstat,  SYS_time, SYS_signal, SYS_execve, SYS_fork,   SYS_link,
+  SYS_unlink, SYS_wait,   SYS_times,  SYS_gettimeofday
+};
+
 static intptr_t syscall_write(int fd, void* buf, size_t count) {
   if (fd == 1 || fd == 2) {
     for (; count; -- count) {
